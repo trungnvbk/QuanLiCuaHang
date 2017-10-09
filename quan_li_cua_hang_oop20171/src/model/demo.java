30a0fdc1f451538144d806e90cd8eddc717c26e5
Trung Nguyen <trungnvbk97@gmail.com>
2017-10-09 17:28:58 +0700

    update

---
+++ quan_li_cua_hang_oop20171/src/model/demo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Trung nguyen");
+    }
+}
